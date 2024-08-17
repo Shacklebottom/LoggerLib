4a105a208a3428d7e8e4f5920bd71b9ff6c39715
Shacklebottom <last0illusion@gmail.com>
2024-08-17 17:41:07 -0500

    dereferenced the project itself and instead referenced the appdirectorymodule dll

---
+++ LoggerModule/DebugDirectory.cs
@@ -0,0 +1,30 @@
+﻿using AppDirectoryModule;
+
+namespace LoggerModule
+{
+    public class DebugDirectory : AppDirectory
+    {
+        public string DebugDir => $"{RootDirectory}\\ProjectLogs";
+
+        public DebugDirectory(string rootDir) : base(rootDir)
+        {
+            CreateAppDirectory(DebugDir);
+        }
+
+        public string GetLastLog()
+        {
+            var logs = GetDirectoryFiles(DebugDir);
+
+            if (logs.Any())
+            {
+                return logs.Last();
+            }
+            return "dummylog_0.txt";
+        }
+
+        public static int GetLogIdentity(string logFile)
+        {
+            return int.Parse(logFile.Split('_').Last().Split('.').First());
+        }
+    }
+}
