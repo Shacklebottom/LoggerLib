578f52a2269c9dabcdb434389285b46ffb638bf4
Shacklebottom <last0illusion@gmail.com>
2024-09-09 11:43:34 -0500

    unloaded the test harness as integration is complete :)

--- TestHarness/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using LoggerModule;
-
-var databaseLogger = new DatabaseLogger();
-
-var message = "Turtles are great, turtles are wonderful!";
-
-databaseLogger.Log(message);
-
-Console.WriteLine($"The turtles have sent a message to the Database!");
